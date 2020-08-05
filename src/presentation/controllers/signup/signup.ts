f20123da277b1e4d1a9d2fcb54f4e279c36939fa
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2020-08-05 20:29:03 -0300

    feat: add ExpressRouteAdapter

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -16,6 +16,8 @@ export class SignUpController implements Controller {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
+          console.log(field)
+          console.log(httpRequest)
           return badRequest(new MissingParamError(field))
         }
       }
